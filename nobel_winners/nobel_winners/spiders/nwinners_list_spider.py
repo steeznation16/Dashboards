d38b1d6cc8301ffea7f3738769b482329ec539ad
steeznation16 <awester17@gmail.com>
2019-04-24 11:49:59 -0400

    update

--- nobel_winners/nobel_winners/spiders/nwinners_list_spider.py
+++ nobel_winners/nobel_winners/spiders/nwinners_list_spider.py
@@ -5,51 +5,11 @@ import re
 
 BASE_URL = 'http://en.wikipedia.org'
 # ...
-def process_winner_li(w, country=None):
-    """
-    Process a winner's <li> tag, adding country of birth or nationality, as applicable.
-    """
-    wdata = {}
-
-    wdata['link'] = BASE_URL + w.xpath('a/@href').extract()[0]
-
-    text = ' '.join(w.xpath('descendant-or-self::text()').extract())
-    # Get comma-delineated name and strip trailing whitespace
-    wdata['name'] = text.split(',')[0].strip()
-
-    year = re.findall('\d{4}', text)
-    if year:
-        wdata['year'] = int(year[0])
-    else:
-        wdata['year'] = 0
-        print("Oops, no year in ", text)
-    
-    category = re.findall(
-        'Physics|Chemistry|Physiology or Medicine|Literature|Peace|Economics', text)
-    if category:
-        wdata['category'] = category[0]
-    else:
-        wdata['category'] = ''
-        print("Oops, no category in ", text)
-    
-    if country:
-        if text.find('*') != -1:
-            wdata['country'] = ''
-            wdata['born_in'] = country
-        else:
-            wdata['country'] = country
-            wdata['born_in'] = ''
-    
-    # store a copy of the link's text string
-    # for any manual corrections
-    wdata['text'] = text
-    return wdata
-
 # A. Define the data to be scraped
 class NWinnerItem(scrapy.Item):
-    country = scrapy.Field()
     name = scrapy.Field()
     link = scrapy.Field()
+    year = scrapy.Field()
     category = scrapy.Field()
     country = scrapy.Field()
     gender = scrapy.Field()
@@ -58,8 +18,7 @@ class NWinnerItem(scrapy.Item):
     date_of_death = scrapy.Field()
     place_of_birth = scrapy.Field()
     place_of_death = scrapy.Field()
-    text = scrapy.Field()   
-    year = scrapy.Field()
+    text = scrapy.Field()
 
 # B. Create a named spider
 class NWinnerSpider(scrapy.Spider):
@@ -72,8 +31,10 @@ class NWinnerSpider(scrapy.Spider):
     ]
     # C. A parse method to deal with the HTTP response
     def parse(self, response):
+
         h3s = response.xpath('//h3')
-        for h3 in list(h3s)[:2]:
+
+        for h3 in list(h3s):
             country = h3.xpath('span[@class="mw-headline"]/text()').extract()
             if country:
                 winners = h3.xpath('following-sibling::ol[1]')
@@ -113,3 +74,69 @@ class NWinnerSpider(scrapy.Spider):
                 item[prop['name']] = sel[0].extract()
         yield item
 
+
+def get_persondata(table, item):
+    fields = ['Date of birth', 'Place of birth', 'Date of death', 'Place of death']
+    for tr in table.xpath('tr'):
+        label = tr.xpath('td[@class="persondata-label"]/text()').extract()
+        if label and label[0] in fields:
+            text = ' '.join(tr.xpath('td[not(@class)]/descendant-or-self::text()').extract())
+            print(text)
+            item[label[0].lower().replace(' ', '_')] = text
+
+
+def guess_gender(text, threshold=0):
+    import re
+
+    he = len(list(re.finditer(' he ', text)))
+    she = len(list(re.finditer(' she ', text)))
+    diff = she - he
+
+    print('she %d, he %d, diff %d'%(she, he, diff))
+    if diff > threshold:
+        return 'female'
+    elif diff < -threshold:
+        return 'male'
+    else:
+        return None
+
+
+def process_winner_li(w, country=None):
+    """
+    Process a winner's <li> tag, adding country of birth or nationality, as applicable.
+    """
+    wdata = {}
+
+    wdata['link'] = BASE_URL + w.xpath('a/@href').extract()[0]
+
+    text = ' '.join(w.xpath('descendant-or-self::text()').extract())
+    # Get comma-delineated name and strip trailing whitespace
+    wdata['name'] = text.split(',')[0].strip()
+
+    year = re.findall('\d{4}', text)
+    if year:
+        wdata['year'] = int(year[0])
+    else:
+        wdata['year'] = 0
+        print("Oops, no year in ", text)
+
+    category = re.findall(
+        'Physics|Chemistry|Physiology or Medicine|Literature|Peace|Economics', text)
+    if category:
+        wdata['category'] = category[0]
+    else:
+        wdata['category'] = ''
+        print("Oops, no category in ", text)
+
+    if country:
+        if text.find('*') != -1:
+            wdata['country'] = ''
+            wdata['born_in'] = country
+        else:
+            wdata['country'] = country
+            wdata['born_in'] = ''
+
+    # store a copy of the link's text string
+    # for any manual corrections
+    wdata['text'] = text
+    return wdata
